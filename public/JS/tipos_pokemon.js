aa3711f7bc64c1359cca899ee39e108a43bcfd20
josueeegl <josuegl12@outlook.com>
2021-09-29 14:56:28 -0600

    v4.5

--- public/JS/tipos_pokemon.js
+++ public/JS/tipos_pokemon.js
@@ -10,7 +10,6 @@ exports.cargar_tipos = function (req, res) {
 exports.Buscar_tipo = function (req, res) {
     const tipo_pokemon = req.body.t_pokemon;
     const pokemonInfos = buscar.buscarxtipos(tipo_pokemon);
-    console.log(pokemonInfos);
     res.render('Pokemonbytype.html', {
         tipo: tipo_pokemon,
         arreglo: pokemonInfos
